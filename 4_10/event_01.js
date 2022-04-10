a20394b7403fc3c3bdce460d81b28263e18ca56e
kalsollrem <kalsollrem@gmail.com>
2022-04-10 23:21:23 +0900

    4월 10일자 이벤트 리스너

---
+++ 4_10/event_01.js
@@ -0,0 +1,18 @@
+let btn1 = document.querySelector("#btn1");
+let btn2 = document.querySelector("#btn2");
+let box = document.getElementById("title_box");
+
+btn1.addEventListener('click', change_bgA);       //적색 백그라운드로 변경
+btn2.addEventListener('click', change_bgB);       //초록색 백그라운드로 변경
+
+function change_bgA()
+{
+    box.style.backgroundColor = "red";
+    alert(빨강);
+}
+
+function change_bgB()
+{
+    box.style.backgroundColor = "green";
+    alert(초록);
+}
