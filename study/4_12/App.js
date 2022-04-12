f5bda1f7045f00e24562087b032ad9070f7c99fd
kalsollrem <kalsollrem@gmail.com>
2022-04-12 23:57:29 +0900

    4월 12

--- study/4_12/App.js
+++ study/4_12/App.js
@@ -37,6 +37,12 @@ function App()
         <button onClick={chTitle}>버튼 </button>
         <hr/>
       </div>
+      <div className='modal'>
+        <h2>제목</h2>
+        <p>날짜</p>
+        <p>상세내용</p>
+      </div>
+
     </div>
 
 
