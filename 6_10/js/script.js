651a89af235150135926823c673dcceae494071c
kalsollrem <kalsollrem@gmail.com>
2022-06-10 23:20:19 +0900

    연습

---
+++ 6_10/js/script.js
@@ -0,0 +1,36 @@
+$(function()
+{
+    $("ul.gnb>li").hover(function(){
+        $(this).find("ul.sub").stop().slideDown();
+    },function(){
+        $(this).find("ul.sub").stop().slideUp();
+    })                                              //내비가이드
+
+    var slideIndex=0;
+    setInterval(function()
+    {
+        if(slideIndex<2)
+        {
+            slideIndex++
+        }
+        else
+        {
+            slideIndex = 0;
+        }
+        $(".main_visual ul").animate({left:(slideIndex*-800)},500)
+    },3000);
+
+    //모달
+    $(".notice ul li").first().click(function()
+    {
+        $(".modal").fadeIn();
+    });
+
+    $(".modal_inner button").first().click(function()
+    {
+        $(".modal").hide();
+    });
+
+
+});
+
